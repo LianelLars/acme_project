7e326d0f8a54b66a61853e48de8dbb67a1c2f84d
LianelLars <lianel.larson@gmail.com>
2023-05-03 14:52:41 -0500

    Lessons 8-10 complete

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -1,8 +1,12 @@
 # birthday/forms.py
 from django import forms
+from django.core.exceptions import ValidationError
 
 # Импортируем класс модели Birthday.
 from .models import Birthday
+
+# Множество с именами участников Ливерпульской четвёрки.
+BEATLES = {'Джон Леннон', 'Пол Маккартни', 'Джордж Харрисон', 'Ринго Старр'}
 
 
 # Для использования формы с моделями меняем класс на forms.ModelForm.
@@ -18,6 +22,24 @@ class BirthdayForm(forms.ModelForm):
             'birthday': forms.DateInput(attrs={'type': 'date'})
         }
 
+    def clean_first_name(self):
+        # Получаем значение имени из словаря очищенных данных.
+        first_name = self.cleaned_data['first_name']
+        # Разбиваем полученную строку по пробелам
+        # и возвращаем только первое имя.
+        return first_name.split()[0]
+
+    def clean(self):
+        # Получаем имя и фамилию из очищенных полей формы.
+        super().clean()
+        first_name = self.cleaned_data['first_name']
+        last_name = self.cleaned_data['last_name']
+        # Проверяем вхождение сочетания имени и фамилии во множество имён.
+        if f'{first_name} {last_name}' in BEATLES:
+            raise ValidationError(
+                'Мы тоже любим Битлз, но введите, пожалуйста, настоящее имя!'
+            )
+
 # class BirthdayForm(forms.Form):
 #     first_name = forms.CharField(label="Имя",
 #                                  max_length=20
@@ -26,5 +48,5 @@ class BirthdayForm(forms.ModelForm):
 #                                 required=False,
 #                                 help_text="Заполнять необязательно")
 #     birthday = forms.DateField(label="Дата рождения",
-#                                widget=forms.DateInput(attrs={'type': 'date'}),
+#                                widget=forms.DateInput(attrs={'type':'date'}),
 #                                )
--- acme_project/birthday/urls.py
+++ acme_project/birthday/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from . import views
@@ -6,5 +8,7 @@ app_name = 'birthday'
 
 urlpatterns = [
     path('', views.birthday, name='create'),
-    path('list/', views.birthday_list, name='list')
-]
+    path('list/', views.birthday_list, name='list'),
+    path('<int:pk>/edit/', views.birthday, name='edit'),
+    path('<int:pk>/delete/', views.delete_birthday, name='delete'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
